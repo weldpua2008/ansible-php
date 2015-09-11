caa04b0f8120c7617454157e6dde746b2d6239c2
Valery Solovyov <weldpua2008@gmail.com>
2015-09-11 21:49:58 +0400

    updated tests

--- tests/test-in-docker-image.sh
+++ tests/test-in-docker-image.sh
@@ -36,18 +36,19 @@ printf "[defaults]\nroles_path = ../" > ansible.cfg
 #
 
 function test_playbook_syntax(){
-    ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} --syntax-check || echo "syntax check was failed" && exit 2
+
+    ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} --syntax-check || (echo "ansible playbook syntax check was failed" && exit 2 )
 }
 
 function test_playbook(){
     # first run
-    ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} ${ANSIBLE_LOG_LEVEL} --connection=local ${SUDO_OPTION} ${ANSIBLE_EXTRA_VARS} || echo "first run was failed" && exit 2
+    ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} ${ANSIBLE_LOG_LEVEL} --connection=local ${SUDO_OPTION} ${ANSIBLE_EXTRA_VARS} || ( echo "first run was failed" && exit 2 )
 
     # Run the role/playbook again, checking to make sure it's idempotent.
     ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} ${ANSIBLE_LOG_LEVEL} --connection=local ${SUDO_OPTION} ${ANSIBLE_EXTRA_VARS} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' ) || (echo 'Idempotence test: fail' && exit 1)
 }
 function extra_tests(){
-    php --version|| echo "php --version was failed" && exit 100
+    php --version|| (echo "php --version was failed" && exit 100 )
 }
 
 function main(){
